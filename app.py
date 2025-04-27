6bbf957ef5427a448cff2078beaba1705abf2756
Mugure Macharia <115866332+mugureeDOTcom@users.noreply.github.com>
2025-04-27 20:09:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import re
 import time
-from serpapi import GoogleSearch  # Correct import
+from serpapi.google_search import GoogleSearch  # Correct import
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
